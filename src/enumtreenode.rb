81a74610844b39e1cf9bf81571039ac20ad790d1
giorno <js@creat.io>
2016-09-03 10:29:33 +0100

    TUI-14 EnumTreeNode behaviour.

---
+++ src/enumtreenode.rb
@@ -0,0 +1,68 @@
+
+# vim: et
+
+require_relative 'modeltreenode'
+
+module Tui
+
+  # Tree node representing a single enum model.
+  class EnumTreeNode < ModelTreeNode
+
+    # Pseudo-model representing a single enum value.
+    class Proxy
+      attr_accessor :model, :label, :ro
+
+      # Constructor.
+      # @param model [EnumProperty] property to proxy
+      # @param option [String] option name
+      public
+      def initialize ( model, option )
+        @model = model
+        @label = option
+        @ro = false # @todo does it ever make sense to be true?
+      end # initialize
+
+      # Inherit the model behaviour.
+      public
+      def to_s
+        return @model.options[@label]
+      end # to_s
+
+    end # Proxy
+
+    # Tree node representing a specific enum value to set it on the enum
+    # model.
+    class ValueTreeNode < ModelTreeNode
+
+      # Set the property value.
+      public
+      def navigate
+        @model.model.value = @model.label
+        @model.label = @model.label
+        @parent.done = true
+        return true
+      end # navigate
+
+    end # ValueTreeNode
+
+    # Repopulate the subtree should the list of options has changed.
+    public
+    def refresh
+      @nodes.clear
+      @model.options.each do |name, value|
+        self << ValueTreeNode.new( Proxy.new( @model, name ), @idr )
+      end
+    end # refresh
+
+    # Revert to the behaviour of the generic tree node.
+    public
+    def navigate
+      # call grandparent since this needs the bahviour of an ordinary tree node
+      Core::TreeNode.instance_method( :navigate ).bind( self ).call
+      return true
+    end # navigate
+
+  end # EnumTreeNode
+
+end # ::Tui
+
--- test/ux/testtreeux.rb
+++ test/ux/testtreeux.rb
@@ -2,9 +2,11 @@
 
 # vim: et
 
-require_relative '../../src/model/string'
 require_relative '../../src/model/integer'
+require_relative '../../src/model/enum'
+require_relative '../../src/model/string'
 require_relative '../../src/integertreenode'
+require_relative '../../src/enumtreenode'
 require_relative '../../src/stringtreenode'
 require_relative '../../src/tree'
 
@@ -13,7 +15,7 @@ require 'pp'
 idr = lambda { |m| if m.respond_to? :label then m.label else m.to_s end }
 tree = Tui::Tree.new( 'variables', idr )
   tree << Tui::IntegerTreeNode.new( Tui::Model::Integer.new( 'ro-id', -1, true ), idr )
-  tree << Tui::ModelTreeNode.new( Tui::Model::String.new( 'enum-model-node', 'e' ), idr )
+  tree << Tui::EnumTreeNode.new( Tui::Model::Enum.new( 'enum-model-node', { 'opt1' => 'val1', 'opt2' => 'val2', 'opt3' => 'val3' }, 'opt1' ), idr )
   tree << Tui::IntegerTreeNode.new( Tui::Model::Integer.new( 'integer-model-node', 42 ), idr )
   tree << Tui::StringTreeNode.new( Tui::Model::String.new( 'string-model-node', 'i' ), idr )
   tree << Tui::ModelTreeNode.new( Tui::Model::String.new( 'struct-model-node', 's' ), idr )
