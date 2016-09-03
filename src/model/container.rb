647c9af76709d68e4392bd23ed3291e9c22cf226
giorno <js@creat.io>
2016-09-03 13:02:36 +0100

    TUI-7 Basic functionality of a Container model.

---
+++ src/model/container.rb
@@ -0,0 +1,40 @@
+
+# vim: et
+
+require_relative 'base'
+
+module Tui module Model
+
+  # Model for container like objects. Essentially a list of objects.
+  class Container < Base
+    attr_accessor :containees
+
+    # Constructor.
+    #
+    # @param label [String] container name
+    public
+    def initialize ( label )
+      super( label )
+      @containees = Array.new
+    end # initialize
+
+    # Value is irrelevant in the container.
+    # @override
+    public
+    def valid? ( value )
+      return true
+    end # valid?
+
+    # Overrides the parent and is expected to be overridden in the subclass.
+    #
+    # @override
+    public
+    def to_s
+      return label.to_s
+    end # to_s
+
+  end # Struct
+
+end # ::Model
+end # ::Tui
+
